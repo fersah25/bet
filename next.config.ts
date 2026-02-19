f4ae9ed5b8f4477fdb2da6058f23f6025a8111b6
fersah25 <152728327+fersah25@users.noreply.github.com>
2026-02-19 18:14:37 +0300

    alayınızı tokatlarım bak

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+import path from 'path';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'encoding': false,
+      '@coinbase/cdp-sdk': path.resolve(__dirname, 'src/empty-module.js'),
+      '@base-org/account': path.resolve(__dirname, 'src/empty-module.js'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
